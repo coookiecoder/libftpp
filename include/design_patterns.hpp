235a8c8c8e8abce945ffc48f88004c49660bdea0
cookie coder <ambroise.bareux@gmail.com>
2025-04-23 12:22:16 +0200

    StateMachine.hpp done

--- include/design_patterns.hpp
+++ include/design_patterns.hpp
@@ -1,3 +1,6 @@
 #pragma once
 
 #include <design_patterns/Memento.hpp>
+#include <design_patterns/Observer.hpp>
+#include <design_patterns/Singleton.hpp>
+#include <design_patterns/StateMachine.hpp>
